e6a2dad18db0e9f154d4ba9bf329fc13885d88e9
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-05-29 14:19:59 -0300

    testando se o projeto funciona

--- src/br/edu/contabanco/main/ContaTerminal.java
+++ src/br/edu/contabanco/main/ContaTerminal.java
@@ -3,7 +3,7 @@ package br.edu.contabanco.main;
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("teste de funcionamento");
 
 	}
 
