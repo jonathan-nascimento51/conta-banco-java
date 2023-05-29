90cc53f6e30da5dad3065dc67f6910db3dd4eb09
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-05-29 14:34:23 -0300

    corpo da classe ContaTerminal

--- src/br/edu/contabanco/main/ContaTerminal.java
+++ src/br/edu/contabanco/main/ContaTerminal.java
@@ -1,9 +1,37 @@
 package br.edu.contabanco.main;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		System.out.println("teste de funcionamento");
+
+		int numeroConta;
+		String agencia;
+		String nomeCliente;
+		Double saldo;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Por favor, insira o numero da conta: ");
+		numeroConta = scanner.nextInt();
+		
+		scanner.nextLine();
+		
+		System.out.println("digite o numero da agencia: ");
+		agencia = scanner.nextLine();;
+		
+		System.out.println("digite seu nome: ");
+		nomeCliente = scanner.nextLine();
+		
+		System.out.println("digite seu saldo: ");
+		saldo = scanner.nextDouble();
+		
+		System.out.println(" ");
+		
+		System.out.println("Obrigado " + nomeCliente + " por criar uma conta conosco, sua agencia é " + agencia + " conta: " +
+		 numeroConta + " e seu saldo é: "+saldo+" o qual ja esta disponivel!");
+		
 
 	}
 
